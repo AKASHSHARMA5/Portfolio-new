c7d97dac9026ce94847fd847de650da26f36eed3
AKASHSHARMA5 <131224911+AKASHSHARMA5@users.noreply.github.com>
2023-05-27 16:38:50 +0530

    add some margin in text

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -6,20 +6,20 @@ export default function Contact() {
       <h1 className='formheading'>Contact me for work/general queries</h1>
       <form>
       <div className="clientname mb-3">
-          <label for="clientname" className="form-label">Name</label>
+          <label htmlFor="clientname" className="form-label">Name</label>
           <input type="text" className="form-control" id="clientname" />
         </div>
         <div className="mb-3">
-          <label for="clientemail" className="form-label">Email address</label>
+          <label htmlFor="clientemail" className="form-label">Email address</label>
           <input type="email" className="form-control" id="clientemail" aria-describedby="emailHelp" />
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="clientphone mb-3">
-          <label for="clientphone" className="form-label">Phone</label>
+          <label htmlFor="clientphone" className="form-label">Phone</label>
           <input type="password" className="form-control" id="clientphone" />
         </div>
         <div className="clientquery mb-3">
-          <label for="clientquery" className="form-label">Query</label>
+          <label htmlFor="clientquery" className="form-label">Query</label>
           <input type="text" className="form-control" id="clientquery" />
         </div>
         <div className='buttons'>
